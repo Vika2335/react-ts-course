aef85050a08f73e5a3f57d87217df793662944ac
Vika2335 <golobokova.viktoriakpc602020@gmail.com>
2024-05-23 13:22:23 +0300

    refactor: onClick

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Card, { CardVariant } from './components/Card';
 const App = () => {
   return (
     <div>
-      <Card onClick={() => console.log('Click')} variant={CardVariant.primary} width='200px' height='200px'>
+      <Card onClick={(num: number) => console.log('Click', num)} variant={CardVariant.primary} width='200px' height='200px'>
         <button>Кнопка</button>
         <div>Ура!</div>
       </Card>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react';
+import React, { FC, ReactNode, useState } from 'react';
 
 export enum CardVariant {
   outLined= 'outlined',
@@ -10,16 +10,17 @@ interface CardProps {
   height?: string;
   variant: CardVariant;
   children?: ReactNode;
-  onClick: () => void;
+  onClick: (num: number) => void;
 }
 
 const Card: FC<CardProps> = ({ width, height, variant, onClick, children }) => {
+  const [state, setState] = useState(0);
   return (
     <div style={{width, height, 
       border: variant === CardVariant.outLined ? '1px solid gray' : 'none',
       background: variant === CardVariant.primary ? 'gray' : 'none'
     }}
-    onClick={onClick}
+    onClick={() => onClick(state)}
     >
       {children}
     </div>
